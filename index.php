d7585975a4b398758a435e0504548d019addb81b
elanfer <elanfer@uni-osnabrueck.de>
2014-12-19 19:07:27 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>CTreffOS RPS Competition</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-encoding" content="gzip"  />
+    <link rel="stylesheet" type="text/css" href="res/global.css" />
+  </head>
+  <body>
+    <div id="nav">
+    </div>
+    <div id="content">
+    </div>
+    <div id="footer">
+    </div>
+  </body>
+</html>
