3d5842fd041ec77fe016c59d445ab03c27f9e024
Timo Osterkamp <timo.osterkamp.42@googlemail.com>
2014-12-21 19:47:29 +0100

    add a tool to create codes

---
+++ tools/codes.py
@@ -0,0 +1,50 @@
+import random
+import sqlite3 as lite
+from sys import argv
+
+
+class sql():
+    __con = None
+    __cur = None
+
+    def close(self):
+        sql.__con.close()
+
+    def connect(self):
+        sql.__con = lite.connect('web.db')
+        sql.__cur = sql.__con.cursor()
+
+    def insert(self, number):
+        sql.__cur.execute('INSERT INTO codes(code) VALUES (?)', (number,))
+        sql.__con.commit()
+
+    def isnew(self, number):
+        sql.__cur.execute('SELECT count(*) FROM codes WHERE code = ?', (number,))
+        if(sql.__cur.fetchall() == 0):
+            return True
+        else:
+            return False
+
+
+class rand():
+
+    def randint(self):
+        return random.randint(100000000000, 999999999999)
+
+    def start(self):
+        while True:
+            number = rand().randint()
+            if(not (sql().isnew(number))):
+                break
+        sql().insert(number)
+
+if __name__ == '__main__':
+    sql().connect()
+    if len(argv) == 2:
+        i = 0
+        while (i < int(argv[1])):
+            rand().start()
+            i = i + 1
+    else:
+        rand().start()
+    sql().close()
